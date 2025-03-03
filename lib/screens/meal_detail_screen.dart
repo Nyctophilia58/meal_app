79e9860f0678f8eba66d923ef80c81fc2ce8b03e
Nyctophilia58 <nowshingdr@protonmail.com>
2025-03-04 00:28:28 +0600

    Improving the meal-detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -13,11 +13,55 @@ class MealDetailScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            for (final ingredient in meal.ingredients)
+              Text(ingredient, style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),),
+            const SizedBox(height: 40),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            for (var i = 0; i < meal.steps.length; i++)
+              Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${i + 1}'),
+                    ),
+                    title: Text(meal.steps[i], style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),),
+                  ),
+                  const Divider(),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
